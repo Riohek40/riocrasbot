32100eb3919fb90da0b5f8a3e0c68c5b72400513
Riohek40 <riohek40@gmail.com>
2025-02-06 02:43:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const global = {
+    owner: [6764248071], // GANTI PAKE ID TELE MU YA GANTENG 
+    botToken: "7636078313:AAFvzN_hNPh2l5QbUa-JIel8UctXe-XeHnU", // ISI BOT TOKEN LU YAK
+}
+
+module.exports = global;
+// Bocoran By Limmvz
+// https://whatsapp.com/channel/0029VawTuv09Gv7VHSUvYi37
